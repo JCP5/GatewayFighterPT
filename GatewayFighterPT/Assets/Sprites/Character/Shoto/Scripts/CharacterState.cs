3119691ac55037a4c427d10ab9cc25d5497edd3a
JCP5 <justen.chong@gmail.com>
2019-11-21 05:59:29 -0600

    First Commit. It's a Doozy

---
+++ GatewayFighterPT/Assets/Sprites/Character/Shoto/Scripts/CharacterState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterState : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public Rigidbody2D rb;
+    public IShotoBase activeState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<Rigidbody2D>() != null)
+            rb = GetComponent<Rigidbody2D>();
+        else
+            Debug.LogError("RigidBody is missing");
+
+        activeState = new Free(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activeState.StateUpdate();
+    }
+}
---
+++ GatewayFighterPT/Assets/Sprites/Character/Shoto/Scripts/Free.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Free : IShotoBase
+{
+    CharacterState manager;
+
+    public Free(CharacterState managerRef)
+    {
+        manager = managerRef;
+    }
+
+    public void StateStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StateUpdate()
+    {
+        Debug.Log(Input.GetAxis("Horizontal"));
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) == 1)
+        {
+            manager.rb.velocity = new Vector2(Input.GetAxis("Horizontal") * manager.moveSpeed * Time.fixedDeltaTime, 0);
+
+            //manager.rb.AddForce(new Vector2((Input.GetAxis("Horizontal")/ Mathf.Abs(Input.GetAxis("Horizontal"))) * manager.moveSpeed * Time.deltaTime, 0), ForceMode2D.Force);
+        }
+        else 
+            manager.rb.velocity = new Vector2(0, 0);
+    }
+}
